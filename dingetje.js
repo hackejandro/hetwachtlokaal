d37aecf00ae0ec956409b54fc2b232c191876955
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2021-07-05 21:00:53 +0200

    Update dingetje.js

--- dingetje.js
+++ dingetje.js
@@ -1,6 +1,6 @@
 
  var $form = $('form#test-form'),
-     url = 'https://script.google.com/macros/s/AKfycbwu9aC8Z65EbuJXtyEmfTMwQksEwsYHMEOnQquXHV2kxECsIV2pjcv7Fy9Y55uErmN0/exec'
+     url = 'https://script.google.com/macros/s/AKfycbzTwAfXTI5OuiZ-p5z5Q1QzTquK19VXFvtbdgYNH1nX6lctUgdBQLuRN-rGH_FTBsGBLw/exec'
  
  $('#submit-form').on('click', function(e) {
    e.preventDefault();
